cf96c57a0f4885b5b2f5d74094f3cc96cb5171c9
styxpilled <styxpilled.dev@gmail.com>
2023-02-06 00:32:52 +0100

    feat: use another post on gallery or video

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -5,8 +5,12 @@ export const load: PageLoad = async ({ fetch }) => {
   const data = await res.json();
 
   try {
-    const url: string = data[0].data.children[0].data.url_overridden_by_dest;
-    return { url };
+    for (const post of data[0].data.children) {
+      if (!(post.data.url_overridden_by_dest as string).includes('gallery') && post.data.post_hint === 'image') {
+        return { url: post.data.url_overridden_by_dest };
+      }
+    }
+    return { url: null };
   } catch (error) {
     return { url: null };
   }
