070dc18a68bac688c4567d2e1a5cfc6230d3ea81
styxpilled <styxpilled.dev@gmail.com>
2023-02-06 18:48:03 +0100

    feat: dark mode and loading indicator

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,6 +19,9 @@
 	<h1>
 		A random Blahaj pulled from <a href="reddit.com/r/BLAHAJ">r/BLAHAJ</a>
 	</h1>
+	{#if !show}
+		loading...
+	{/if}
 	<img bind:this={img} class:hidden={!show} alt="Random Blahaj image" />
 </main>
 
@@ -28,6 +31,25 @@
 		display: flex;
 		flex-direction: column;
 		align-items: center;
+	}
+
+	:global(html) {
+		height: 100vh;
+		width: 100vw;
+	}
+
+	@media (prefers-color-scheme: dark) {
+		:global(html) {
+			color: aliceblue;
+			background-color: #181a1b;
+		}
+	}
+
+	@media (prefers-color-scheme: light) {
+		:global(html) {
+			color: black;
+			background-color: aliceblue;
+		}
 	}
 
 	img {
